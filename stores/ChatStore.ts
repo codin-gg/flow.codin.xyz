637121a5c701a3bb780d546635c6ff316c494e0f
Christian <crifei93@gmail.com>
2023-04-24 18:26:51 +0000

    remove audio

--- stores/ChatStore.ts
+++ stores/ChatStore.ts
@@ -2,30 +2,13 @@ import { create } from "zustand";
 import { Message } from "./Message";
 import { persist } from "zustand/middleware";
 import { Chat } from "./Chat";
-import { SpeechRecognizer } from "microsoft-cognitiveservices-speech-sdk";
-import type { AudioChunk } from "./PlayerActions";
-
 export type APIState = "idle" | "loading" | "error";
-export type AudioState = "idle" | "recording" | "transcribing" | "processing";
 
 export const excludeFromState = [
   "currentAbortController",
-  "recorder",
-  "recognizer",
-  "recorderTimeout",
   "textInputValue",
   "apiState",
-  "audioState",
-  "submitNextAudio",
-  "audioChunks",
-  "ttsID",
-  "ttsText",
   "activeChatId",
-  "playerRef",
-  "playerRefInit",
-  "playerState",
-  "playerAudioQueue",
-  "playerIdx",
 ];
 
 interface SettingsForm {
@@ -40,18 +23,6 @@ interface SettingsForm {
   frequency_penalty: number;
   logit_bias: string;
   auto_title: boolean;
-  // Whisper
-  auto_detect_language: boolean;
-  spoken_language: string;
-  spoken_language_code: string;
-  // ElevenLabs
-  voice_id: string;
-  // Azure
-  voice_id_azure: string;
-  auto_detect_language_azure: boolean;
-  spoken_language_azure: string;
-  spoken_language_code_azure: string;
-  spoken_language_style: string;
   submit_debounce_ms: number;
 }
 
@@ -66,27 +37,12 @@ export const defaultSettings = {
   frequency_penalty: 0,
   logit_bias: "",
   auto_title: true,
-  // Whisper
-  auto_detect_language: false,
-  spoken_language: "English (en)",
-  spoken_language_code: "en",
-  // ElevenLabs
-  voice_id: "21m00Tcm4TlvDq8ikWAM",
-  // Azure
-  voice_id_azure: "en-US-JaneNeural",
-  auto_detect_language_azure: true,
-  spoken_language_azure: "English (US)",
-  spoken_language_code_azure: "en-US",
-  spoken_language_style: "friendly",
   submit_debounce_ms: 0,
 };
 
 export interface ChatState {
   apiState: APIState;
   apiKey: string | undefined;
-  apiKey11Labs: string | undefined;
-  apiKeyAzure: string | undefined;
-  apiKeyAzureRegion: string | undefined;
 
   chats: Chat[];
   activeChatId: string | undefined;
@@ -96,39 +52,13 @@ export interface ChatState {
   defaultSettings: SettingsForm;
   navOpened: boolean;
 
-  pushToTalkMode: boolean;
-  recorder: MediaRecorder | undefined;
-  recognizer: SpeechRecognizer | undefined;
-  recorderTimeout: ReturnType<typeof setTimeout> | undefined;
-  submitNextAudio: boolean;
-  audioState: AudioState;
-  audioChunks: BlobPart[];
-  playerMode: boolean;
   editingMessage: Message | undefined;
-
-  ttsID: string | undefined;
-  ttsText: string | undefined;
-  playerRef: React.MutableRefObject<HTMLAudioElement | null>;
-  playerRefInit: boolean;
-  playerIdx: number;
-  playerState: "playing" | "paused" | "idle";
-  playerApiState: APIState;
-  playerAudioQueue: AudioChunk[];
-
-  showTextDuringPTT: boolean;
-  autoSendStreamingSTT: boolean;
   modelChoicesChat: string[] | undefined;
-  modelChoiceTTS: string | undefined;
-  modelChoiceSTT: string | undefined;
   textInputValue: string;
 }
 export const initialState = {
   apiState: "idle" as APIState,
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY || undefined,
-  apiKey11Labs: process.env.NEXT_PUBLIC_11LABS_API_KEY || undefined,
-  apiKeyAzure: process.env.NEXT_PUBLIC_AZURE_API_KEY || undefined,
-  apiKeyAzureRegion: process.env.NEXT_PUBLIC_AZURE_REGION || undefined,
-
   chats: [],
   activeChatId: undefined,
   colorScheme: "light" as "light" | "dark",
@@ -136,31 +66,9 @@ export const initialState = {
   settingsForm: defaultSettings,
   defaultSettings: defaultSettings,
   navOpened: false,
-  playerMode: false,
-  pushToTalkMode: false,
   editingMessage: undefined,
-
-  recorder: undefined,
-  recognizer: undefined,
-  recorderTimeout: undefined,
-  submitNextAudio: true,
-  audioState: "idle" as AudioState,
-  audioChunks: [],
-  showTextDuringPTT: true,
-  ttsID: undefined,
-  ttsText: undefined,
-  playerRef: { current: null },
-  playerRefInit: false,
-  playerIdx: -1,
-  playerState: "idle",
-  playerApiState: "idle",
-  playerAudioQueue: [],
-
-  autoSendStreamingSTT: true,
   modelChoicesChat: undefined,
   modelChoiceChat: undefined,
-  modelChoiceTTS: "azure",
-  modelChoiceSTT: "azure",
   textInputValue: "",
 };
 
